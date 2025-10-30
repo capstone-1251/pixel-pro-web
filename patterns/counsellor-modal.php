6bdf51d5842de4d0cbb6579301102b632c0f5e7f
SankalpaPoudelGIT <spoudel4@nait.ca>
2025-10-30 14:58:09 -0600

    Add counsellor modal block pattern
    Introduces a new block pattern for a counsellor modal, featuring a YouTube video embed, counsellor profile section, informational columns, and a booking button.

---
+++ patterns/counsellor-modal.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Title: Counsellor Modal
+ * Slug: pixel-pro-web/counsellor-modal
+ * Categories: text, featured
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Counsellor Modal"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:embed {"url":"https://www.youtube.com/watch?v=u31qwQUeGuM","type":"video","providerNameSlug":"youtube","responsive":true,"align":"wide","className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+    <figure class="wp-block-embed alignwide is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+        <div class="wp-block-embed__wrapper">
+            https://www.youtube.com/watch?v=u31qwQUeGuM
+        </div>
+    </figure>
+    <!-- /wp:embed -->
+
+    <!-- wp:columns {"align":"wide"} -->
+    <div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.33%"} -->
+        <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group"><!-- wp:image {"id":431,"width":"221px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+                <figure class="wp-block-image size-full is-resized"><img src="http://test-site.local/wp-content/uploads/2025/10/Portrait_Placeholder.png" alt="Portrait of [Counsellor Name]" class="wp-image-431" style="width:221px;height:auto" /></figure>
+                <!-- /wp:image -->
+
+                <!-- wp:heading -->
+                <h2 class="wp-block-heading">Counsellor Name</h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph -->
+                <p><strong>Lorem ipsum</strong></p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"width":"66.66%"} -->
+        <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Lorem Ipsum</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Lorem Ipsum</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Lorem Ipsum</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Lorem Ipsum</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+            <div class="wp-block-buttons"><!-- wp:button -->
+                <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Book With This Counsellor</a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
