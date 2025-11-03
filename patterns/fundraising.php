aa4d19713389c1a28fee259f4ea99d0c40faebe4
StarsInTech <130418684+StarsInTech@users.noreply.github.com>
2025-11-03 08:14:07 -0700

    saved the pattern

---
+++ patterns/fundraising.php
@@ -0,0 +1,70 @@
+<!-- wp:group {"metadata":{"name":"Main"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+
+            <!-- wp:paragraph -->
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit
+                quisque faucibus.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph -->
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
+                placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
+                integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per
+                conubia nostra inceptos himenaeos.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:heading -->
+            <h2 class="wp-block-heading">Heading</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:list -->
+            <ul class="wp-block-list"><!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+
+                <!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+
+                <!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+
+                <!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+
+                <!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+
+                <!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+
+                <!-- wp:list-item -->
+                <li>List Item</li>
+                <!-- /wp:list-item -->
+            </ul>
+            <!-- /wp:list -->
+
+            <!-- wp:buttons -->
+            <div class="wp-block-buttons"><!-- wp:button -->
+                <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Donate Now</a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
