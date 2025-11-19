d323998b183e03bc9422989bd50c16f60c2b483f
StarsInTech <130418684+StarsInTech@users.noreply.github.com>
2025-11-18 18:02:16 -0700

    updated the missing metadata for the updated hero sections

--- patterns/hero-no-button-updated.php
+++ patterns/hero-no-button-updated.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * Title: Hero no button updated
+ * Slug: pixel-pro-web/hero-no-button-updated
+ * Categories: hero, featured
+ * Block Types: core/group
+ * Inserter: true
+ */
+?>
 <!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"isDark":false,"metadata":{"name":"Hero no button updated","categories":["hero"],"patternName":"pixel-pro-web/hero-no-button-updated"},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover is-light"><span aria-hidden="true"
         class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span>
